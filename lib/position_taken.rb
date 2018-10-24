d83c9726e2cdb5fdbe55c3bb15d4f3bc0a21de08
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 19:12:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken? (board, index)
-  index == " "
-    return false
   index == "X" || "O"
     return true
+  index == " "
+      return false
 
 end
 # code your #position_taken? method here!

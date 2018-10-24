3e47437fc2b9a1dace5df2ab8e02bc62d156b85f
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 19:40:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,19 @@
+def position_taken? (board, index)
+  if board[index] == " "
+    return false
+  else board[index] == "X" || "O"
+    return true
+
+end
+
 
 
 def position_taken?(board,position)
-  if board[position] == " " || "" || nil
+  if board[position] == " " || "" || 
+    false
+    elsif board[position] == ""
+    false
+    elsif  board[position] == nil
     false
     else   board[position] == "X" || "O"
     true

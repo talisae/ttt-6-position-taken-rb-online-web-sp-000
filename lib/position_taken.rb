7493e3ee4e16f42a9e03ad013332acf001a87886
Talisa Erazo <talisaerazo@gmail.com>
2018-10-24 19:30:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken? (board, index = "X")
   board[index] == " "
     return false
-  board[index] == "X" || index == "O"
+  board[index] == "X" || board[index] == "O"
     return true
 
 end
